4da7f823e43755345890d620ccd9054e184afb80
Alroy Fernandes <alroyfernandes07518@gmail.com>
2022-06-23 21:10:08 +0530

    Header compoenet created

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { ReactComponent as Logo } from "../../assets/logo.svg"
+import "./headerStyles.css"
+
+
+import { AiOutlineMinus } from "react-icons/ai"
+import { RiCheckboxBlankLine } from "react-icons/ri"
+import { IoMdClose } from "react-icons/io"
+const Header: React.FC = () => {
+    return (
+        <div className="HeaderContainer">
+            <div className="LogoContainer">
+                <Logo />
+            </div>
+
+            <div className="RightIconContainer">
+                <AiOutlineMinus className="RightIcons" />
+                <RiCheckboxBlankLine className="RightIcons" />
+                <IoMdClose className="RightIcons" style={{ fontSize: 22 }} />
+            </div>
+        </div>
+    )
+}
+
+export default Header
