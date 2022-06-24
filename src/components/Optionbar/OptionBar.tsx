e53dd0a2286396c93e3777507130bd4fc6fb8fb3
Alroy Fernandes <alroyfernandes07518@gmail.com>
2022-06-24 13:31:17 +0530

    added a dropdwon

--- src/components/Optionbar/OptionBar.tsx
+++ src/components/Optionbar/OptionBar.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 import "./optionbarStyles.css"
 import { ReactComponent as FilterOne } from "../../assets/FilterOne.svg"
 import { ReactComponent as FilterTwo } from "../../assets/FilterTwo.svg"
+import {
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
 
+    Button,
+} from '@chakra-ui/react'
 import { BsChevronDown } from "react-icons/bs"
 const OptionBar: React.FC = () => {
     return (
@@ -15,11 +22,28 @@ const OptionBar: React.FC = () => {
                     <div className="YearDiv">
                         <FilterOne /> <span className="FilterTerm">Year</span> <BsChevronDown className="BottomIcon" />
                     </div>
-                    <div className="SortDiv">
-                        <FilterTwo />
-                        <span className="FilterTerm">A-Z</span>
-                        <BsChevronDown className="BottomIcon" />
-                    </div>
+
+                    <Menu>
+                        {({ isOpen }) => (
+                            <>
+                                <div className="SortDiv">
+                                    <FilterTwo />
+
+                                    <MenuButton isActive={isOpen} as={Button}>
+                                        <span className="FilterTerm">A-Z</span>
+                                    </MenuButton>
+                                    <BsChevronDown className="BottomIcon" />
+                                </div>
+
+                                <MenuList>
+                                    <MenuItem>Amount High - Low</MenuItem>
+                                    <MenuItem>Amount Low - High</MenuItem>
+                                    <MenuItem>Arrange A-Z</MenuItem>
+                                    <MenuItem>Arrange Z-A</MenuItem>
+                                </MenuList>
+                            </>
+                        )}
+                    </Menu>
                 </div>
             </div>
         </div>
